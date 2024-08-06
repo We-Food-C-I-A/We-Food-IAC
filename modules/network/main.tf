0606ab9bb0a11fa54e2b3b000cde1d93abd09beb
Jinwoo Lee <binaryWooo@gmail.com>
2024-08-06 20:20:51 +0900

    feat: 단일 인스턴스 구축 #3

---
+++ modules/network/main.tf
@@ -0,0 +1,216 @@
+# vpc 리소스 정의
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.cidr_block
+  enable_dns_support   = "true"
+  enable_dns_hostnames = "true"
+  instance_tenancy     = "default"
+
+  tags = {
+    Name = "${var.region_name}-${var.terraform_name}-vpc"
+  }
+}
+
+# vpc에서 사용되는 internet_gateway 정의
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "${var.region_name}-${var.terraform_name}-igw"
+  }
+}
+
+# # NAT Gateway에 할당할 IP
+# resource "aws_eip" "ngw_ip" {
+#   domain = "vpc"
+# }
+
+# # NAT Gateway 선언
+# resource "aws_nat_gateway" "ngw" {
+#   allocation_id = aws_eip.ngw_ip.id
+#   subnet_id     = aws_subnet.net.id
+# }
+
+# net subnet 정의(nat, bastion이 이에 속함)
+resource "aws_subnet" "net" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = cidrsubnet(var.cidr_block, 8, 0)
+  availability_zone = "ap-northeast-2a"
+
+  tags = {
+    Name = "${var.region_name}-${var.terraform_name}-net-public"
+  }
+}
+
+# net subnet2 정의(nat, bastion이 이에 속함)
+resource "aws_subnet" "net2" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = cidrsubnet(var.cidr_block, 8, 3)
+  availability_zone = "ap-northeast-2b"
+
+  tags = {
+    Name = "${var.region_name}-${var.terraform_name}-net-public"
+  }
+}
+
+# app subnet 정의(web, was가 이에 속함)
+resource "aws_subnet" "app" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = cidrsubnet(var.cidr_block, 8, 1)
+  availability_zone = "ap-northeast-2a"
+
+  tags = {
+    Name = "${var.region_name}-${var.terraform_name}-app-private"
+  }
+}
+
+# data subnet 정의(rds, elasticcache 등이 이에 속함)
+resource "aws_subnet" "data" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = cidrsubnet(var.cidr_block, 8, 2)
+  availability_zone = "ap-northeast-2a"
+
+  tags = {
+    Name = "${var.region_name}-${var.terraform_name}-data-private"
+  }
+}
+
+# net subnet에서 사용되는 route_table 정의
+resource "aws_route_table" "net" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.region_name}-${var.terraform_name}-net-rtb"
+  }
+}
+
+# net subnet과 route_table을 연결
+resource "aws_route_table_association" "net" {
+  subnet_id      = aws_subnet.net.id
+  route_table_id = aws_route_table.net.id
+}
+
+# app subnet에서 사용되는 route_table 정의
+resource "aws_route_table" "app" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "${var.region_name}-${var.terraform_name}-app-rtb"
+  }
+}
+
+# resource "aws_route" "outbound_nat_route" {
+#   route_table_id         = aws_route_table.app.id
+#   destination_cidr_block = "0.0.0.0/0"
+#   nat_gateway_id         = aws_nat_gateway.ngw.id
+# }
+
+# app subnet과 route_table을 연결
+resource "aws_route_table_association" "app" {
+  subnet_id      = aws_subnet.app.id
+  route_table_id = aws_route_table.app.id
+}
+
+# bastion 서버의 security-group 정의
+resource "aws_security_group" "bastion_sg" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "${var.region_name}-${var.terraform_name}-bastion-sg"
+  }
+}
+
+# bastion 서버에서 net 서브넷에서 오는 접근 허용
+resource "aws_security_group_rule" "bastion_inbound_ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = [var.remote_ip]
+  security_group_id = aws_security_group.bastion_sg.id
+}
+
+# bastion 서버에서 http 접근 허용
+resource "aws_security_group_rule" "http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.bastion_sg.id
+}
+
+# bastion 서버에서 https 접근 허용
+resource "aws_security_group_rule" "https" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.bastion_sg.id
+}
+
+# bastion 서버에서 vpn 접근 허용
+resource "aws_security_group_rule" "ovpn" {
+  type              = "ingress"
+  from_port         = 1194
+  to_port           = 1194
+  protocol          = "udp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.bastion_sg.id
+}
+
+# bastion 서버에서 net 서브넷에서 오는 접근 허용
+resource "aws_security_group_rule" "vpc_inbound" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = [cidrsubnet(var.cidr_block, 8, 0)]
+  security_group_id = aws_security_group.bastion_sg.id
+}
+
+# bastion 서버의 모든 outbound 허용
+resource "aws_security_group_rule" "bastion_outbound" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.bastion_sg.id
+}
+
+# AWS AMI 이미지 데이터 검색
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+# bastion 서버(EC2) 생성
+resource "aws_instance" "bastion" {
+  ami                         = "ami-00e557080ac814181"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.net.id
+  vpc_security_group_ids      = [aws_security_group.bastion_sg.id]
+  associate_public_ip_address = true
+  key_name                    = var.keypair
+
+  tags = {
+    Name = "${var.region_name}-${var.terraform_name}--bastion"
+  }
+}
---
+++ modules/network/variables.tf
@@ -0,0 +1,24 @@
+# vpc에서 사용되는 cidr_block
+variable "cidr_block" {
+  type = string
+}
+
+# 리소스명에서 참조되는 region_name
+variable "region_name" {
+  type = string
+}
+
+# 리소스명에서 참조되는 terraform_name(서비스명)
+variable "terraform_name" {
+  type = string
+}
+
+# bastion에 접근하기 위해 사용되는 키 쌍
+variable "keypair" {
+  type = string
+}
+
+# bastion에 접근 가능한 ip
+variable "remote_ip" {
+  type = string
+}
