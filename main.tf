111e30badd7420687f2e00c6cb91d987b6801ffa
Jinwoo Lee <binaryWooo@gmail.com>
2024-08-06 20:27:09 +0900

    VPC 및 단일 인스턴스 환경 구축 close #3 (#4)
    * feat: vpc 환경 구축을 위한 변수 추가 #3
    
    * feat: 단일 인스턴스 구축 #3
    
    * feat: 단일 인스턴스 구축을 위한 모듈 호출 #3
    
    * docs: 인스턴스 접속 방법 작성 #3
    
    * fix: 단일 인스턴스 이름 수정 #3
    
    * docs: chmod 명령어 권한 수정

--- main.tf
+++ main.tf
@@ -4,3 +4,13 @@ provider "aws" {
   secret_key = var.secret_key
   region     = var.region
 }
+
+# VPC, Subnet 등 네트워크 환경 구축
+module "network" {
+  source         = "./modules/network"
+  cidr_block     = var.cidr_block
+  region_name    = var.region_name
+  terraform_name = var.terraform_name
+  keypair        = var.keypair
+  remote_ip      = var.remote_ip
+}
